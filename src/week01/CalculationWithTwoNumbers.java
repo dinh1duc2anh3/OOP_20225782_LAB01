bd556ede589b67cc995c8023cce1c2adfed7318d
dinh1duc2anh3 <dinh1duc2anh3@gmail.com>
2024-09-29 21:20:54 +0700

    add code result illustration images
    nothing

--- src/week01/CalculationWithTwoNumbers.java
+++ src/week01/CalculationWithTwoNumbers.java
@@ -2,7 +2,7 @@ package week01;
 
 import javax.swing.JOptionPane;
 
-public class CalculatationWith2Numbers {
+public class CalculationWithTwoNumbers{
 	public static void main(String[] args) {
 		String strNum1,strNum2;
 		String strSum = " Sum of two numbers is ";
